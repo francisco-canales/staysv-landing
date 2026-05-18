860f5cb7db2dffe0add571d40b038f88c995bd22
francisco-canales <canalesf23@gmail.com>
2026-05-17 20:08:12 -0600

    Agregando validacion de formulario

--- script.js
+++ script.js
@@ -21,3 +21,57 @@ window.addEventListener("scroll", () => {
 
 });
 
+// VALIDACION DEL FORMULARIO
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    let nombre = document.getElementById("nombre").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let mensaje = document.getElementById("mensaje").value.trim();
+
+    let errorNombre = document.getElementById("errorNombre");
+    let errorEmail = document.getElementById("errorEmail");
+    let errorMensaje = document.getElementById("errorMensaje");
+
+    let success = document.getElementById("success");
+
+    errorNombre.textContent = "";
+    errorEmail.textContent = "";
+    errorMensaje.textContent = "";
+    success.textContent = "";
+
+    let valido = true;
+
+    if(nombre === ""){
+        errorNombre.textContent = "Ingrese su nombre";
+        valido = false;
+    }
+
+    let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(email === ""){
+        errorEmail.textContent = "Ingrese su correo";
+        valido = false;
+    }else if(!regexEmail.test(email)){
+        errorEmail.textContent = "Correo inválido";
+        valido = false;
+    }
+
+    if(mensaje === ""){
+        errorMensaje.textContent = "Ingrese el destino turístico";
+        valido = false;
+    }
+
+    if(valido){
+
+        success.textContent =
+        "Reserva enviada correctamente. Nos comunicaremos pronto.";
+
+        formulario.reset();
+    }
+
+});
